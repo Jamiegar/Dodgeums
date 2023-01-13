90cd04aa60aae2efa9566a8856448c196f0f8980
Jamie Linnell <jamiegar99@gmail.com>
2023-01-13 11:16:03 +0000

    Initial commit

---
+++ Source/Dodgeums/DodgeumsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DodgeumsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DODGEUMS_API ADodgeumsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
